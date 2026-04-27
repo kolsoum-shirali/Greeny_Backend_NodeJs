9e3c5546118b78b095b310510c71bda45847fdc9
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-27 07:57:52 -0600

    update 5 files

--- src/routes/auth/controller.js
+++ src/routes/auth/controller.js
@@ -1,4 +1,4 @@
-const controller = require("./../controller");
+const controller = require("../controller");
 const _ = require("lodash");
 const config = require("config");
 const jwt = require("jsonwebtoken");
--- src/routes/auth/validator.js
+++ src/routes/auth/validator.js
@@ -2,13 +2,13 @@ const expressValidator = require("express-validator");
 const check = expressValidator.check;
 
 module.exports = new (class {
-  registerValidator() {
-    return (
-      [check("email").isEmail().withMessage("Email is Invalid")],
-      [check("name").not().isEmpty("name can't be empty")],
-      [check("password").not().isEmpty("password can't be empty")]
-    );
-  }
+  // registerValidator() {
+  //   return (
+  //     [check("email").isEmail().withMessage("Email is Invalid")],
+  //     [check("name").not().isEmpty("name can't be empty")],
+  //     [check("password").not().isEmpty("password can't be empty")]
+  //   );
+  // }
   loginValidator() {
     return (
       [check("email").isEmail().withMessage("Email is Invalid")],
