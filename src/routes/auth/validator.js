cdb23c0d4879eefac8d92acbbb0e579d773d0788
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-27 09:07:27 -0600

    update validator.js

--- src/routes/auth/validator.js
+++ src/routes/auth/validator.js
@@ -2,17 +2,19 @@ const expressValidator = require("express-validator");
 const check = expressValidator.check;
 
 module.exports = new (class {
-  // registerValidator() {
-  //   return (
-  //     [check("email").isEmail().withMessage("Email is Invalid")],
-  //     [check("name").not().isEmpty("name can't be empty")],
-  //     [check("password").not().isEmpty("password can't be empty")]
-  //   );
-  // }
+  registerValidator() {
+    return [
+      check("email").isEmail().withMessage("Email is invalid"),
+
+      check("name").notEmpty().withMessage("Name can't be empty"),
+
+      check("password").notEmpty().withMessage("Password can't be empty"),
+    ];
+  }
   loginValidator() {
-    return (
-      [check("email").isEmail().withMessage("Email is Invalid")],
-      [check("password").not().isEmpty("password can't be empty")]
-    );
+    return [
+      check("email").isEmail().withMessage("Email is invalid"),
+      check("password").notEmpty().withMessage("Password can't be empty"),
+    ];
   }
 })();
