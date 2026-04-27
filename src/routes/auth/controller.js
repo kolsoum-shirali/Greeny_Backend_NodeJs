b8345421eb22b8f6900859e7a75f15a0e49da73f
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-27 09:07:22 -0600

    update controller.js

--- src/routes/auth/controller.js
+++ src/routes/auth/controller.js
@@ -7,31 +7,47 @@ const bcrypt = require("bcrypt");
 module.exports = new (class extends controller {
   async register(req, res) {
     try {
-      let user = this.UserModel.findOne({ email: req.body.email });
+      let user = await this.UserModel.findOne({ email: req.body.email });
+
       if (user) {
+        // User already exists
         return this.response({
           res: res,
           code: 400,
-          message: "this user already registered",
+          message: "This user already registered",
         });
       }
 
+      // Create a new user instance
       user = new this.UserModel(
-        _.pick(req.body, ["name", "email", "password"])
+        _.pick(req.body, ["name", "email", "password"]),
       );
-      // convert pass to hash by bcrypt package
+
+      // Hash the password using bcrypt
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(user.password, salt);
+
+      // Save the new user to the database
       await user.save();
+
+      // Send a success response
       this.response({
         res,
-        message: "the user successfuly registered",
+        message: "The user successfully registered",
         data: _.pick(user, ["_id", "name", "email"]),
       });
     } catch (err) {
-      console.log(err);
+      // 5. Improved error handling
+      console.error("Registration error:", err); // Log the error for debugging
+      // Send a generic server error response to the client
+      this.response({
+        res: res,
+        code: 500,
+        message: "An internal server error occurred during registration.",
+      });
     }
   }
+
   async login(req, res) {
     try {
       const user = await this.UserModel.findOne({ email: req.body.email });
