8d5e3a38ca18766fa1a3352ce7b23c5818c55eda
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-04-27 23:37:31 -0600

    update controller.js

--- src/routes/controller.js
+++ src/routes/controller.js
@@ -1,32 +1,36 @@
 const autoBind = require("auto-bind");
 const { validationResult } = require("express-validator");
 const UserModel = require("../models/user");
+const ProductModel = require("../models/product");
+
 module.exports = class {
   constructor() {
     autoBind(this);
     this.UserModel = UserModel;
+    this.ProductModel = ProductModel;
   }
-validationBody(req, res) {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    const errors = result.array();
-    const messages = errors.map(err => err.msg);
+  validationBody(req, res) {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      const errors = result.array();
+      const messages = errors.map((err) => err.msg);
 
-    res.status(400).json({
-      message: "validation error",
-      data: messages,
-    });
-    return false; // indicates error
+      res.status(400).json({
+        message: "validation error",
+        data: messages,
+      });
+      return false; // indicates error
+    }
+    return true;
   }
-  return true;
-}
 
-validate(req, res, next) {
-  const isValid = this.validationBody(req, res);
-  if (!isValid) return; // stop if not valid
+  validate(req, res, next) {
+    const isValid = this.validationBody(req, res);
+    if (!isValid) return; // stop if not valid
 
-  next();
-}  response({ res, message, code = 200, data = {} }) {
+    next();
+  }
+  response({ res, message, code = 200, data = {} }) {
     res.status(code).json({
       message,
       data,
