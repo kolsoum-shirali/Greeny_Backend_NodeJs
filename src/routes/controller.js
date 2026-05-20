5a4423002fdff4676a79d70ed30d65529694ab9a
kolsoum-shirali <kolsoumshirali2002@gmail.com>
2026-05-20 08:12:57 -0600

    update controller.js

--- src/routes/controller.js
+++ src/routes/controller.js
@@ -1,7 +1,7 @@
 const autoBind = require("auto-bind");
 const { validationResult } = require("express-validator");
 const UserModel = require("../models/user");
-const OrderModel = require("../models/order");
+const OrderModel = require("../models/order")
 
 module.exports = class {
   constructor() {
